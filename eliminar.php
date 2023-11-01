9a5606a4e7e7ed14be7cc148c5fa6dd4121ce720
userincos <tiwanakunocturno@gmail.com>
2023-10-31 20:13:03 -0400

    cambios en eliminar

--- eliminar.php
+++ eliminar.php
@@ -4,11 +4,15 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+  
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>eliminar</title>
 </head>
+<head>
+    a
+  </head>
     <body>
 
       <!--Hard delete: eliminar desde la base de datos-->
